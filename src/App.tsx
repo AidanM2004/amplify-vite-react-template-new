329d3a7f2a37a862db10bb62fd1c5c66c6eb1e67
Aidan Milholland <amilholland22@gmail.com>
2025-05-19 19:01:39 -0400

    maybe??

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,7 @@ const client = generateClient<Schema>();
 
 function App() {
   const { user, signOut } = useAuthenticator();
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
- 
   
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
 
   return (
     <main>
