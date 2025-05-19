657cb034e934620180af6c9016c70d4b47603a61
Aidan Milholland <amilholland22@gmail.com>
2025-05-19 19:09:19 -0400

    who knows

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
 import { useAuthenticator } from '@aws-amplify/ui-react';
-
-const client = generateClient<Schema>();
-
 
 function App() {
   const { user, signOut } = useAuthenticator();
-  
 
   return (
     <main>
-      <h1>Thank you {user?.signInDetails?.loginId?.split('@')[0].replace(/\d+/g, '')} for signing up</h1>
+      <h1>
+        Thank you {user?.signInDetails?.loginId?.split('@')[0].replace(/\d+/g, '')} for signing up
+      </h1>
       <button onClick={signOut}>Sign out</button>
     </main>
   );
